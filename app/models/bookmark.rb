105b191d1ec957ddb08a1acf0457f5224aaa6c8e
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-01-16 15:02:17 +0100

    models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   validates :comment, presence: true, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
-  validates :movie, presence: true
+  validates :movie, presence: true, uniqueness: { scope: :list_id, message: "This movie is already in the list" }
   validates :list, presence: true
 end
