a0d2142ec606a277e524fcedff81db374314e922
Mathieu COQUELET <coqueletmathieu@gmail.com>
2025-01-16 17:54:19 +0100

    movie cards

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,5 @@
 class BookmarksController < ApplicationController
   def new
-    @movies = Movie.all
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
@@ -22,6 +21,7 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
+    raise
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 
